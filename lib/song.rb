dd53d0aaeaaf735bbe6dfb2f44460e101aa6514e
Adam <inmon.adam@yahoo.com>
2022-01-29 16:17:57 -0600

    commit tree 193047276570311e61f72002cb26e31658630046

---
+++ lib/song.rb
@@ -0,0 +1,40 @@
+
+class Song
+
+    @@count = 0
+    @@artists = []
+    @@genres = []
+
+    attr_accessor :name, :artist, :genre
+    
+
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@count += 1
+        @@artists << artist
+        @@genres << genre
+    end
+
+    def self.count
+        @@count
+    end
+
+    def self.artists
+        @@artists.uniq
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    def self.genre_count
+        @@genres.group_by(&:itself).map { |key, value| [key, value.count] }.to_h
+    end
+
+    def self.artist_count
+        @@artists.group_by(&:itself).map { |key, value| [key, value.count] }.to_h
+    end
+
+end
